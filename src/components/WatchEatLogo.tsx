b54b729207fd6215c7db1e409adf340c28486a45
WatchEat <bitebuddyorstenlynuriel@gmail.com>
2025-09-11 10:31:12 +0300

    Updated WatchEatLogo.tsx

--- src/components/WatchEatLogo.tsx
+++ src/components/WatchEatLogo.tsx
@@ -5,7 +5,7 @@ type Props = { className?: string };
 export default function WatchEatLogo({ className = "" }: Props) {
   return (
     <img
-      src="/watcheat-logo.png"
+      src="/image.png"
       alt="WatchEat logo"
       className={`block select-none pointer-events-none mix-blend-normal filter-none ${className}`}
       draggable={false}
